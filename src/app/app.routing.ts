692f1d4e7026f152d6b9c05ce936944a9dbcee03
Matthias De Bie <mattydebie@gmail.com>
2018-05-23 15:18:50 +0200

    Removed unnecessary components, cleaned style

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -55,10 +55,6 @@ export const routes: Routes = [
         loadChildren: './views/base/base.module#BaseModule'
       },
       {
-        path: 'buttons',
-        loadChildren: './views/buttons/buttons.module#ButtonsModule'
-      },
-      {
         path: 'charts',
         loadChildren: './views/chartjs/chartjs.module#ChartJSModule'
       },
@@ -67,21 +63,9 @@ export const routes: Routes = [
         loadChildren: './views/dashboard/dashboard.module#DashboardModule'
       },
       {
-        path: 'icons',
-        loadChildren: './views/icons/icons.module#IconsModule'
-      },
-      {
         path: 'notifications',
         loadChildren: './views/notifications/notifications.module#NotificationsModule'
       },
-      {
-        path: 'theme',
-        loadChildren: './views/theme/theme.module#ThemeModule'
-      },
-      {
-        path: 'widgets',
-        loadChildren: './views/widgets/widgets.module#WidgetsModule'
-      }
     ]
   }
 ];
