a8c84791109abb9560cace008a040555806c74eb
Lucas Pauwels <pauwelslucas2@gmail.com>
2018-06-06 15:30:50 +0200

    Fixes 37373: Added Cypress testing for actions

--- src/app/views/dashboard/actions/actions.component.ts
+++ src/app/views/dashboard/actions/actions.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, Input, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ESFilter} from '../filter/ESFilter';
+import {saveAs} from 'file-saver';
 
 @Component({
   selector: 'app-actions',
@@ -71,5 +72,64 @@ export class ActionsComponent implements OnInit {
       this.totalActions = res['total'];
       this.actions = res['hits'];
     });
+  }
+
+  getActionsForTest() {
+    this.checkFilters();
+
+    this.httpClient.get('https://localhost:5000/action?' + ESFilter.createQueryParams(this.activeFilters) + '',
+      {
+        headers: this.httpHeaders
+      }).toPromise().then(res => {
+      this.createCypressTest(res['hits'] as Array<Object>);
+    });
+  }
+
+  checkFilters() {
+    let bool = false;
+    for (const filter of this.activeFilters) {
+      if (filter['value'].toLowerCase() === 'req' && filter['includes'] === 'include') {
+        filter['includes'] = 'exclude';
+        bool = true;
+      } else if (filter['value'].toLowerCase() === 'req') {
+        return;
+      }
+    }
+
+    if (bool === false) {
+      this.activeFilters.push(new ESFilter('method', 'exclude', 'REQ'));
+    }
+  }
+
+  createCypressTest(testActions) {
+    testActions.reverse();
+    const cypressActions = [];
+    cypressActions.push('describe(\'Sportoffice login\', function () {\n' +
+      '    it("Gets, types and asserts", function () {');
+    const url = 'https://sportoase-multi-uat.inuits.eu' + testActions[0]._source.path;
+    cypressActions.push('cy.visit(\'' + url + '\');');
+    for (const action of testActions) {
+
+      if (action._source.tree === '') {
+        cypressActions.push('cy.get(\'' + action._source.type + '\').contains(\'' + action._source.value + '\').click();');
+      } else {
+        if (action._source.method === 'focusout') {
+          if (action._source.value !== '') {
+            cypressActions.push('cy.get(\'' + action._source.tree + '\').type(\'' +
+              action._source.value + '\').should(\'have.value\', \'' + action._source.value + '\');');
+          } else {
+            cypressActions.push('cy.get(\'' + action._source.tree + '\');');
+          }
+        } else {
+          cypressActions.push('cy.get(\'' + action._source.tree + '\').click();');
+        }
+      }
+    }
+
+    cypressActions.push('});\n});');
+
+    if (cypressActions !== []) {
+      saveAs(new Blob([cypressActions.join('\n')], {type: 'text'}), 'cypressTest.js');
+    }
   }
 }
